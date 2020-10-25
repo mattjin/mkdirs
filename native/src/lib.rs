125fa4815ce8e928c1697a6b62c216da4734f6b0
jing.jin <jing.jin@gongbangbang.com>
2020-10-25 17:21:54 +0800

    add async fun

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,13 +2,43 @@ use neon::prelude::*;
 use std::fs;
 use neon::register_module;
 
-fn mkdirs(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+struct DirTask {
+  argument: String,
+}
+
+impl Task for DirTask {
+  type Output = usize;
+  type Error = ();
+  type JsEvent = JsUndefined;
+
+  fn perform(&self) -> Result<usize, ()> {
+      fs::create_dir_all(&self.argument).unwrap();
+      Ok(1)
+  }
+
+  fn complete(self, mut cx: TaskContext, _result: Result<usize, ()>) -> JsResult<JsUndefined> {
+      Ok(cx.undefined())
+  }
+}
+
+fn mkdirs_sync(mut cx: FunctionContext) -> JsResult<JsUndefined> {
   let str = cx.argument::<JsString>(0)?.value();
   fs::create_dir_all(&str).unwrap();
   Ok(cx.undefined())
 }
 
+fn mkdirs_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+  let str = cx.argument::<JsString>(0)?.value();
+  let cb = cx.argument::<JsFunction>(1)?;
+
+  let task = DirTask { argument: str };
+  task.schedule(cb);
+
+  Ok(cx.undefined())
+}
+
 register_module!(mut cx, {
-  cx.export_function("mkdirs", mkdirs)?;
+  cx.export_function("mkdirsSync", mkdirs_sync)?;
+  cx.export_function("mkdirsAsync", mkdirs_async)?;
   Ok(())
 });
